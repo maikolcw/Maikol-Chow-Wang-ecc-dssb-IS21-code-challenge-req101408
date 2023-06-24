21a25ef644540081060bb3a6d72abcba88cc196c
maikolcw <whysoraccoony@gmail.com>
2023-06-24 16:17:43 -0700

    added general get all products route and tested return values

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const { writeFile, readFile } = require('fs')
+const util = require('util')
+const writeFileAsync = util.promisify(writeFile)
+const readFileAsync = util.promisify(readFile)
 
 const app = express()
 const port = 3000
+// holds the products info from data.json in memory for manipulation
+var productsJSON = []
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, async () => {
+    try {
+        productsJSON = await readFileAsync('./data.json', 'utf-8')
+        if (!productsJSON) {
+            console.log("Could not read the file");
+            return
+        }
+        productsJSON = JSON.parse(productsJSON)
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(`Example app listening on port ${port} and is ready to go`)
 }
 )
 
 app.get('/api', (req, res) => {
     res.send('API is healthy!')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(productsJSON)
 })
 
 app.post('/api', (req, res) => {
