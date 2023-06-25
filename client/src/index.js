13b4600febb245cfae89cfc751931d4d121011f4
maikolcw <whysoraccoony@gmail.com>
2023-06-24 19:20:03 -0700

    started client side

---
+++ client/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function Main() {
+
+  return (
+    <>
+      <table className='table-auto'>
+        <thead>
+          <tr>
+            <th className='border-2'>Product ID</th>
+            <th className='border-2'>Product Name</th>
+            <th className='border-2'>Product Owner Name</th>
+            <th className='border-2'>Developers</th>
+            <th className='border-2'>Scrum Master Name</th>
+            <th className='border-2'>Start Date</th>
+            <th className='border-2'>Methodology</th>
+            <th className='border-2'>Location</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Main />
+);
